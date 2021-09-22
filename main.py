a25b011359be1688018038b7adbf5c83d761a11f
Pedro <pedroneto12092000@hotmail.com>
2021-09-22 14:26:29 -0300

    connection worked without exceptions

--- main.py
+++ main.py
@@ -7,22 +7,33 @@ from ocpp.v16 import ChargePoint as cp
 from ocpp.v16.enums import Action, RegistrationStatus
 from ocpp.v16 import call_result
 
-
 class ChargePoint(cp):
     @on(Action.BootNotification)
     def on_boot_notitication(self, charge_point_vendor, charge_point_model, **kwargs):
+        print(charge_point_vendor)
+        print('bn')
         return call_result.BootNotificationPayload(
             current_time=datetime.utcnow().isoformat(),
             interval=10,
             status=RegistrationStatus.accepted
         )
 
+    @on(Action.StatusNotification)
+    def on_status_notitication(self, status, connector_id, **params):
+        print(status)
+        print(connector_id)
+        return call_result.StatusNotificationPayload(
+        )
+
+    @on(Action.Heartbeat)
+    def on_heart_beat(self):
+        print('heartbeat')
+        return call_result.HeartbeatPayload(
+            current_time=datetime.utcnow().isoformat(),
+        )
+
 
 async def on_connect(websocket, path):
-    """ For every new charge point that connects, create a ChargePoint instance
-    and start listening for messages.
-
-    """
     charge_point_id = path.strip('/')
     cp = ChargePoint(charge_point_id, websocket)
 
@@ -32,7 +43,7 @@ async def on_connect(websocket, path):
 async def main():
     server = await websockets.serve(
         on_connect,
-        '0.0.0.0',
+        '192.168.0.242',
         9000,
         subprotocols=['ocpp1.6']
     )
