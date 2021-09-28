4c0d4fd2660f9faa8f3eab86988ed7389ed713b2
Pedro <pedroneto12092000@hotmail.com>
2021-09-28 16:56:10 -0300

    better logs and function to send message

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ from datetime import datetime
 
 from ocpp.routing import on
 from ocpp.v16 import ChargePoint as cp
-from ocpp.v16.enums import Action, RegistrationStatus
-from ocpp.v16 import call_result
+from ocpp.v16.enums import Action, RegistrationStatus, ResetStatus, ResetType
+from ocpp.v16 import call_result, call
+
+hb = 0
 
 class ChargePoint(cp):
     @on(Action.BootNotification)
-    def on_boot_notitication(self, charge_point_vendor, charge_point_model, **kwargs):
-        print(charge_point_vendor)
-        print('bn')
+    def on_boot_notitication(self, **kwargs):
+        print('\n\n')
+        print('BOOT NOTIFICATION')
+        print('charge_point_vendor = '+kwargs.get('charge_point_vendor'))
+        print('charge_point_model = '+kwargs.get('charge_point_model'))
+        print('\n\n')
         return call_result.BootNotificationPayload(
             current_time=datetime.utcnow().isoformat(),
             interval=10,
@@ -19,18 +24,33 @@ class ChargePoint(cp):
         )
 
     @on(Action.StatusNotification)
-    def on_status_notitication(self, status, connector_id, **params):
-        print(status)
-        print(connector_id)
+    def on_status_notitication(self, **kwargs):
+        print('\n\n')
+        print('STATUS NOTIFICATION')
+        print('status = '+kwargs.get('status'))
+        print('connector_id = '+str(kwargs.get('connector_id')))
+        print('error_code = '+str(kwargs.get('error_code')))
+        print('\n\n')
         return call_result.StatusNotificationPayload(
         )
 
     @on(Action.Heartbeat)
     def on_heart_beat(self):
-        print('heartbeat')
+        global hb
+        print('heartbeat ' + str(hb))
+        hb += 1
         return call_result.HeartbeatPayload(
             current_time=datetime.utcnow().isoformat(),
         )
+
+    async def reset_send(self):
+        request = call.ResetPayload(
+            type=ResetType.soft
+        )
+        response = await self.call(request)
+        if response.status == ResetStatus.accepted:
+            print("Reset Started!!!")
+
 
 
 async def on_connect(websocket, path):
